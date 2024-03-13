bc9df657479200259e6bfd5f187bdd7d93ef0f00
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2024-03-13 12:21:15 -0700

    changed open times

--- src/index.js
+++ src/index.js
@@ -117,8 +117,8 @@ function Pizza(props) {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 2;
-  const closeHour = 24;
+  const openHour = 13;
+  const closeHour = 21;
   const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
 
@@ -132,12 +132,7 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>
-            We're open untill {closeHour}:00. Come visit us or order online.
-          </p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00
@@ -145,6 +140,17 @@ function Footer() {
       )}
     </footer>
   );
+}
+
+function Order(props) {
+  <div className="order">
+    <div className="order">
+      <p>
+        We're open untill {props.closeHour}:00. Come visit us or order online.
+      </p>
+      <button className="btn">Order</button>
+    </div>
+  </div>;
 }
 
 // React v18
